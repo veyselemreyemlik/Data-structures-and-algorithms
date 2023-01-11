d103ed9b5d7cfd817841ed2914525b896f414d25
Veysel Emre Yemlik <60979094+veyselemreyemlik@users.noreply.github.com>
2023-01-11 17:53:32 +0300

    Add files via upload

---
+++ Pointer C++/Proje4_2Hafta/Proje4_2Hafta.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+    using namespace std;
+    int main() {
+        int var = 5;
+
+        // Isaretci degisken bildiriyoruz.
+        int* pointVar;
+
+        pointVar = &var;
+
+        // var deeri yazdiriyoruz.
+        cout << "var = " << var << endl;
+
+        // var adresini yazdırıyoruz
+        cout << "Address of var (&var) = " << &var << endl
+            << endl;
+
+        // pointVar ı yazdiriyoruz.
+        cout << "pointVar = " << pointVar << endl;
+
+        // pointVar ın isaret ettigi adres adresinin icerigini yazdırıyoruz.
+        cout << "Content of the address pointed to by pointVar (*pointVar) = " << *pointVar << endl;
+
+        return 0;
+   
+}
+
+
