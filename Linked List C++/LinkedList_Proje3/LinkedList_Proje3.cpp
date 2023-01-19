0625199b8ab801b71c5dff9e606518b7eedf56f0
Veysel Emre Yemlik <60979094+veyselemreyemlik@users.noreply.github.com>
2023-01-19 17:22:05 +0300

    Add files via upload

---
+++ Linked List C++/LinkedList_Proje3/LinkedList_Proje3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+//Yeni bir node oluşturuyoruz.
+class Node {
+public:
+    double data;
+    Node* next;
+public:
+    Node(){}
+    void ekle(int);
+   
+}; Node* head;
+
+void Node::ekle(int sayi) {
+    Node* newlist = new Node;     // eklenecek nense için yer açılır.
+    newlist->data = sayi;         // data ve next pointer lar atanır.
+    newlist->next = 0;
+    Node* p = head;             // listede dolaşıcak bir pointer tanımlanır.
+    if (p == 0)
+        head = newlist;
+    else
+    {
+        while (p->next != 0)
+            p = p->next;
+        p->next = newlist;    // en sona gelindiğinde eleman eklenmiş olur.
+
+    }
+}
+
+
+int main()
+{
+    Node a;
+    int i;
+    cout << "Eklemek Istediğiniz Sayi :" << endl << endl;
+    cin >> i;
+    a.ekle(i);
+    
+}
